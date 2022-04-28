d300aeecd3f1a4a9915023025157a1d47838d3cf
DongwookKim0823 <dwkim0823@naver.com>
2022-04-29 08:54:35 +0900

    8주차(2)

---
+++ src/main/java/com/example/demo/src/user/model/GetUserInfoRes.java
@@ -0,0 +1,21 @@
+package com.example.demo.src.user.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class GetUserInfoRes {
+    private String nickName;
+    private String name;
+    private String profileImgUrl;
+    private String website;
+    private String introduction;
+    private int followerCount;
+    private int followingCount;
+    private int postCount;
+
+}
---
+++ src/main/java/com/example/demo/src/user/model/GetUserPostsRes.java
@@ -0,0 +1,15 @@
+package com.example.demo.src.user.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class GetUserPostsRes {
+    private int postIdx;
+    private String postImgUrl;
+
+}
